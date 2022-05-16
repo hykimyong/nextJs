c4326f77a2af71428d8a07e1d98a269925eeb4ff
hykimyong <hykimyong@naver.com>
2022-05-16 22:07:52 +0900

    nextjs예시
    react처럼 상단에 import를 해주지 않아도 사용이 가능하다

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    "hi"
-  )
+  <div>
+    <h1>Hello</h1>
+  </div>
+  );
 }
