f562835792de8ad45df941eb0fd07b824752e114
hykimyong <hykimyong@naver.com>
2022-06-12 18:34:30 +0900

    jsx를 이용하여 css설정 주기

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,15 @@
 import NavBar from "../components/Navbar"
 
 export default function Home() {
-  return (
-  <div>
-    <NavBar/>
-    <h1>Hello</h1>
-  </div>
-  );
-}
+   return (
+      <div>
+        <NavBar />
+        <h1 className="active">Hello</h1>
+        <style jsx>{`
+          a {
+            color: white;
+          }
+        `}</style>
+      </div>
+    );
+  }
